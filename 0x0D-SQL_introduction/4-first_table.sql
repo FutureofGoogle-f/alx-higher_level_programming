93954c8e4baa99f58d175da63f7256fa929b4cdd
Fikir <fikiralex243@gmail.com>
2024-01-17 00:03:22 -0800

    0x0D-SQL_introduction

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- a script that creates a table 
+CREATE TABLE first_table IF NOT EXISTS (id INT, name VARCHAR(256));
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- a script that displays the number of records
+SELECT * FROM first_table where id = 89;
