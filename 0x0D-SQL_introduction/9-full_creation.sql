7bbc3c0c164549e0affd1ff0360351608437a654
Fikir <fikiralex243@gmail.com>
2024-01-17 00:42:17 -0800

    0x0D-SQL_introduction

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- a script that creates a table second_table
+CREATE TABLE second_table IF NOT EXISTS (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table(id, name, score) VALUES(1, 'John',10), (2, 'Alex', 3), (4,'Bob', 14), (4, 'George', 8);
